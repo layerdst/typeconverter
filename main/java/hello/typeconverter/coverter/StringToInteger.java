b17a545cfe0b797f5efc14c2d31feb4eff444931
layerdst <bccfcc@hanmail.net>
2022-10-30 20:12:50 +0900

    typeconverter init

---
+++ main/java/hello/typeconverter/coverter/StringToInteger.java
@@ -0,0 +1,13 @@
+package hello.typeconverter.coverter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+
+@Slf4j
+public class StringToInteger implements Converter<String, Integer> {
+    @Override
+    public Integer convert(String source) {
+      log.info("convert source={}", source);
+      return Integer.valueOf(source);
+    }
+}
