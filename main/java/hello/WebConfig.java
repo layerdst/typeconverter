ef0ebc618f76ef7d4ac0bb43e2034e05d3f73fe8
layerdst <bccfcc@hanmail.net>
2022-11-01 20:01:26 +0900

    spring webconfigurer converter setting

---
+++ main/java/hello/WebConfig.java
@@ -0,0 +1,22 @@
+package hello;
+
+import hello.typeconverter.coverter.IntegerToStringConverter;
+import hello.typeconverter.coverter.IpPortToStringConverter;
+import hello.typeconverter.coverter.StringToInteger;
+import hello.typeconverter.coverter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry){
+        registry.addConverter(new StringToInteger());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+
+}
--- main/java/hello/typeconverter/controller/HelloController.java
+++ main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @GetMapping("/hello-v1")
